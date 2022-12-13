7e4fb69b3e57f7b48ba1881ebf94a9a68a664266
James Devereux <devereuxjj@gmail.com>
2022-12-13 22:27:32 +0900

    basic app complete

---
+++ src/services/firebase.js
@@ -0,0 +1,71 @@
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  serverTimestamp,
+  onSnapshot,
+  query,
+  orderBy,
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCUvzE0ZjbuZz6TwkBZBBcI0nJE99shAK8",
+  authDomain: "react-chat-app-b34ef.firebaseapp.com",
+  projectId: "react-chat-app-b34ef",
+  storageBucket: "react-chat-app-b34ef.appspot.com",
+  messagingSenderId: "397378306378",
+  appId: "1:397378306378:web:77756bbb403ea119d401b3",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function sendMessage(roomId, user, text) {
+  try {
+    await addDoc(collection(db, "chat-rooms", roomId, "messages"), {
+      uid: user.uid,
+      displayName: user.displayName,
+      text: text.trim(),
+      timestamp: serverTimestamp(),
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function loginWithGoogle() {
+  try {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+
+    const { user } = await signInWithPopup(auth, provider);
+
+    return { uid: user.uid, displayName: user.displayName };
+  } catch (error) {
+    if (error.code !== "auth/cancelled-popup-request") {
+      console.error(error);
+    }
+
+    return null;
+  }
+}
+
+function getMessages(roomId, callback) {
+  return onSnapshot(
+    query(
+      collection(db, "chat-rooms", roomId, "messages"),
+      orderBy("timestamp", "asc")
+    ),
+    (querySnapshot) => {
+      const messages = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      callback(messages);
+    }
+  );
+}
+
+export { loginWithGoogle, sendMessage, getMessages };
